1923a0217e501c826f435de0c0a48f7151e143c9
yasufumi <yasufumi.taniguchi@gmail.com>
2019-08-21 09:10:42 +0900

    Add bucket and sort method

--- torchpipe/core.py
+++ torchpipe/core.py
@@ -13,19 +13,52 @@ from torch.utils.data import get_worker_info
 class Dataset(IterableDataset):
     def __init__(self, dataset: Iterable[Any]) -> None:
         assert isinstance(dataset, _collections_abc.Iterable)
+
+        if isinstance(dataset, _collections_abc.Sequence):
+            self._get_iterator = self._get_iterator_from_sequence
+        elif isinstance(dataset, easyfile.TextFile):
+            self._get_iterator = self._get_iterator_from_easyfile
+        else:
+            self._get_iterator = self._get_iterator_from_iterable
+
         self._dataset = dataset
 
     def __iter__(self) -> Iterator[Any]:
+        yield from self._get_iterator()
+
+    def _get_iterator_from_iterable(self) -> Iterator[Any]:
         worker_info = get_worker_info()
-        it, self._dataset = itertools.tee(self._dataset)
+        iterable, self._dataset = itertools.tee(self._dataset)
         if worker_info is None:
-            yield from it
+            yield from iterable
         else:
             worker_id = worker_info.id
             num_workers = worker_info.num_workers
-            for i, x in enumerate(it):
-                if i % num_workers == worker_id:
-                    yield x
+            yield from itertools.islice(iterable, worker_id, None, num_workers)
+
+    def _get_iterator_from_sequence(self) -> Iterator[Any]:
+        worker_info = get_worker_info()
+        if worker_info is None:
+            yield from self._dataset
+        else:
+            worker_id = worker_info.id
+            num_workers = worker_info.num_workers
+            span = math.ceil(len(self._dataset) / num_workers)
+            start = span * worker_id
+            end = span * (worker_id + 1)
+            yield from self._dataset[start:end]
+
+    def _get_iterator_from_easyfile(self) -> Iterator[str]:
+        worker_info = get_worker_info()
+        if worker_info is None:
+            yield from self._dataset
+        else:
+            worker_id = worker_info.id
+            num_workers = worker_info.num_workers
+            span = math.ceil(len(self._dataset) / num_workers)
+            start = span * worker_id
+            end = span * (worker_id + 1)
+            yield from self._dataset.iterate(start, end)
 
     def all(self) -> List[Any]:
         return list(self)
@@ -50,8 +83,14 @@ class Dataset(IterableDataset):
     def filter(self, predicate: Callable[[Any], bool]) -> 'FilterDataset':
         return FilterDataset(self, predicate)
 
+    def bucket(self, bucket_size: int) -> 'BucketDataset':
+        return BucketDataset(self, bucket_size)
+
     def shuffle(self, buffer_size: int = None) -> 'ShuffleDataset':
         return ShuffleDataset(self, buffer_size)
+
+    def sort(self, sort_key: Callable, buffer_size: int = None) -> 'SortDataset':
+        return SortDataset(self, sort_key, buffer_size)
 
     def window(self, window_size: int, shift: int = None) -> 'WindowDataset':
         return WindowDataset(self, window_size, shift)
@@ -67,44 +106,6 @@ class Dataset(IterableDataset):
     @staticmethod
     def range(*args: Tuple[int]) -> 'RangeDataset':
         return RangeDataset(*args)
-
-
-class EasyfileDataset(Dataset):
-    def __init__(self, dataset: easyfile.TextFile) -> None:
-        assert isinstance(dataset, easyfile.TextFile)
-
-        self._dataset = dataset
-
-    def __iter__(self):
-        worker_info = get_worker_info()
-        if worker_info is None:
-            yield from self._dataset
-        else:
-            worker_id = worker_info.id
-            num_workers = worker_info.num_workers
-            span = math.ceil(len(self._dataset) / num_workers)
-            start = span * worker_id
-            end = span * (worker_id + 1)
-            yield from self._dataset.iterate(start, end)
-
-
-class SequenceDataset(Dataset):
-    def __init__(self, dataset: lf.core.DatasetMixin) -> None:
-        assert isinstance(dataset, lf.core.DatasetMixin)
-
-        self._dataset = dataset
-
-    def __iter__(self):
-        worker_info = get_worker_info()
-        if worker_info is None:
-            yield from self._dataset
-        else:
-            worker_id = worker_info.id
-            num_workers = worker_info.num_workers
-            span = math.ceil(len(self._dataset) / num_workers)
-            start = span * worker_id
-            end = span * (worker_id + 1)
-            yield from self._dataset[start:end]
 
 
 class ApplyDataset(Dataset):
@@ -151,6 +152,24 @@ class FilterDataset(Dataset):
         yield from lf.filter(self._predicate, self._dataset, lazy=True)
 
 
+class BucketDataset(Dataset):
+    def __init__(self, dataset: Dataset, bucket_size: int) -> None:
+        assert isinstance(dataset, Dataset)
+
+        self._bucket_size = bucket_size
+        self._dataset = dataset
+
+    def __iter__(self) -> Iterator[Any]:
+        bucket = []
+        for x in self._dataset:
+            bucket.append(x)
+            if len(bucket) == self._bucket_size:
+                yield bucket
+                bucket = []
+        if bucket:
+            yield bucket
+
+
 class ShuffleDataset(Dataset):
     def __init__(self, dataset: Dataset, buffer_size: int = None) -> None:
         assert isinstance(dataset, Dataset)
@@ -166,17 +185,41 @@ class ShuffleDataset(Dataset):
                 chunk.append(x)
             random.shuffle(chunk)
             yield from chunk
-            return
-
-        for x in self._dataset:
-            chunk.append(x)
-            if len(chunk) >= self._buffer_size:
+        else:
+            for x in self._dataset:
+                chunk.append(x)
+                if len(chunk) == self._buffer_size:
+                    random.shuffle(chunk)
+                    yield from chunk
+                    chunk = []
+            if chunk:
                 random.shuffle(chunk)
                 yield from chunk
-                chunk = []
-        if chunk:
-            random.shuffle(chunk)
-            yield from chunk
+
+
+class SortDataset(Dataset):
+    def __init__(self, dataset: Dataset, sort_key: Callable, buffer_size: int = None) -> None:
+        assert isinstance(dataset, Dataset)
+        assert callable(sort_key)
+
+        self._dataset = dataset
+        self._sort_key = sort_key
+        self._buffer_size = buffer_size
+
+    def __iter__(self) -> Iterator[Any]:
+        if self._buffer_size is None:
+            yield from sorted(self._dataset, key=self._sort_key)
+        else:
+            chunk = []
+            for x in self._dataset:
+                chunk.append(x)
+                if len(chunk) == self._buffer_size:
+                    chunk.sort(key=self._sort_key)
+                    yield from chunk
+                    chunk = []
+            if chunk:
+                chunk.sort(key=self._sort_key)
+                yield from chunk
 
 
 class RangeDataset(Dataset):
