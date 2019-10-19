d59814812c1f4efb5087afaa5500355c3fdc7b2b
yasufumi <yasufumi.taniguchi@gmail.com>
2019-10-19 23:27:22 +0900

    Update test cases

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,8 +1,6 @@
 from unittest import TestCase
-import tempfile
 from itertools import chain
 
-import easyfile
 from torch.utils.data import DataLoader
 
 from torchpipe import Dataset
@@ -12,7 +10,7 @@ class DatasetTestCase(TestCase):
 
     def setUp(self):
         self.base = range(100)
-        self.data = Dataset.range(100)
+        self.data = Dataset(self.base)
 
     def test_dunder_iter(self):
         dataset = Dataset(self.base)
@@ -21,12 +19,24 @@ class DatasetTestCase(TestCase):
                 self.assertEqual(x, y)
 
     def test_iterates_with_dataloader(self):
-        loader = DataLoader(Dataset(self.base),
+        loader = DataLoader(self.data.parallel().shuffle(11),
+                            batch_size=16,
+                            num_workers=0,
+                            collate_fn=lambda x: x)
+        with self.assertWarns(RuntimeWarning):
+            self.assertSequenceEqual(sorted(x for batch in loader for x in batch),
+                                     self.base)
+
+    def test_iterates_with_dataloader_in_parallel(self):
+        loader = DataLoader(self.data.parallel().shuffle(11),
                             batch_size=16,
                             num_workers=2,
                             collate_fn=lambda x: x)
         self.assertSequenceEqual(sorted(x for batch in loader for x in batch),
                                  self.base)
+
+    def test_all(self):
+        self.assertListEqual(self.data.all(), list(self.base))
 
     def test_apply(self):
         def f(it):
@@ -39,16 +49,13 @@ class DatasetTestCase(TestCase):
         for x, y in zip(self.data.apply(f), expected):
             self.assertEqual(x, y)
 
-    def test_map(self):
-        def f(x): return x ** 2
+    def test_batch(self):
+        for i, x in enumerate(self.data.batch(3)):
+            self.assertEqual(x, list(self.base[i * 3: i * 3 + 3]))
 
-        for x, y in zip(self.data.map(f), map(f, self.base)):
-            self.assertEqual(x, y)
-
-    def test_filter(self):
-        def f(x): return x % 2 == 0
-
-        for x, y in zip(self.data.filter(f), filter(f, self.base)):
+    def test_concat(self):
+        other = Dataset(range(100))
+        for x, y in zip(self.data.concat(other), chain(self.base, self.base)):
             self.assertEqual(x, y)
 
     def test_flat_map(self):
@@ -57,9 +64,34 @@ class DatasetTestCase(TestCase):
         for x, y in zip(self.data.flat_map(f), chain.from_iterable(map(f, self.base))):
             self.assertEqual(x, y)
 
-    def test_bucket(self):
-        for i, x in enumerate(self.data.bucket(3)):
-            self.assertEqual(x, list(self.base[i * 3: i * 3 + 3]))
+    def test_filter(self):
+        def f(x): return x % 2 == 0
+
+        for x, y in zip(self.data.filter(f), filter(f, self.base)):
+            self.assertEqual(x, y)
+
+    def test_first(self):
+        self.assertEqual(self.data.first(), self.base[0])
+
+    def test_map(self):
+        def f(x): return x ** 2
+
+        for x, y in zip(self.data.map(f), map(f, self.base)):
+            self.assertEqual(x, y)
+
+    def test_shard(self):
+        n = 4
+        for i in range(n):
+            for x, y in zip(self.data.shard(n, i), self.base[i::n]):
+                self.assertEqual(x, y)
+
+    def test_shuffles_data_with_buffer(self):
+        for x, y in zip(sorted(self.data.shuffle(3)), self.base):
+            self.assertEqual(x, y)
+
+    def test_shuffles_data_without_buffer(self):
+        for x, y in zip(sorted(self.data.shuffle()), self.base):
+            self.assertEqual(x, y)
 
     def test_sorts_data_with_buffer(self):
         expected = chain.from_iterable(reversed(self.base[i: i + 3])
@@ -71,20 +103,6 @@ class DatasetTestCase(TestCase):
         for x, y in zip(self.data.sort(lambda x: -x),  reversed(self.base)):
             self.assertEqual(x, y)
 
-    def test_shuffles_data_with_buffer(self):
-        for x, y in zip(sorted(self.data.shuffle(3)), self.base):
-            self.assertEqual(x, y)
-
-    def test_shuffles_data_without_buffer(self):
-        for x, y in zip(sorted(self.data.shuffle()), self.base):
-            self.assertEqual(x, y)
-
-    def test_all(self):
-        self.assertListEqual(self.data.all(), list(self.base))
-
-    def test_first(self):
-        self.assertEqual(self.data.first(), self.base[0])
-
     def test_take(self):
         n = 50
         self.assertListEqual(self.data.take(n), list(self.base[:n]))
@@ -94,127 +112,6 @@ class DatasetTestCase(TestCase):
             self.assertEqual(x, y)
 
     def test_zip(self):
-        other = Dataset.range(100)
+        other = Dataset(range(100))
         for x, i in zip(self.data.zip(other), self.base):
             self.assertTupleEqual(x, (i, i))
-
-    def test_concat(self):
-        other = Dataset.range(100)
-        for x, y in zip(self.data.concat(other), chain(self.base, self.base)):
-            self.assertEqual(x, y)
-
-
-class IterableDatasetTestCase(TestCase):
-
-    def setUp(self):
-        self.base = iter(range(100))
-        self.data = Dataset(iter(range(100)))
-
-    def test_dunder_iter(self):
-        for x, y in zip(self.data, self.base):
-            self.assertEqual(x, y)
-
-    def test_iterates_with_dataloader(self):
-        dataset = DataLoader(self.data,
-                             batch_size=16,
-                             num_workers=2,
-                             shuffle=False,
-                             collate_fn=lambda x: x)
-        for x, y in zip(sorted(x for batch in dataset for x in batch), self.base):
-            self.assertEqual(x, y)
-
-
-class SequenceDatasetTestCase(TestCase):
-
-    def setUp(self):
-        self.base = list(range(100))
-        self.data = Dataset(self.base)
-
-    def test_dunder_iter(self):
-        for x, y in zip(self.data, self.base):
-            self.assertEqual(x, y)
-
-    def test_iterates_with_dataloader(self):
-        dataset = DataLoader(self.data,
-                             batch_size=16,
-                             num_workers=2,
-                             shuffle=False,
-                             collate_fn=lambda x: x)
-        self.assertSequenceEqual(sorted(x for batch in dataset for x in batch),
-                                 self.base)
-
-
-class EasyfileDatasetTestCase(TestCase):
-
-    def setUp(self):
-        self.fp = tempfile.NamedTemporaryFile()
-        self.n = 100
-        for i in range(self.n):
-            self.fp.write(f'line #{str(i).zfill(3)}\n'.encode('utf-8'))
-        self.fp.seek(0)
-        self.data = Dataset(easyfile.TextFile(self.fp.name))
-
-    def tearDown(self):
-        self.fp.close()
-
-    def test_dunder_iter(self):
-        for i, x in enumerate(self.data):
-            self.assertEqual(x, f'line #{str(i).zfill(3)}')
-
-    def test_iterates_with_dataloader(self):
-        dataset = DataLoader(self.data,
-                             batch_size=16,
-                             num_workers=2,
-                             shuffle=False,
-                             collate_fn=lambda x: x)
-        for i, x in enumerate(sorted(x for batch in dataset for x in batch)):
-            self.assertEqual(x, f'line #{str(i).zfill(3)}')
-
-
-class RangeDatasetTestCase(TestCase):
-
-    def setUp(self):
-        self.end = 100
-
-    def test_dunder_init(self):
-        d1 = Dataset.range(self.end)
-        d2 = Dataset.range(50, self.end)
-        d3 = Dataset.range(0, self.end, 3)
-        self.assertTupleEqual(d1._args, (self.end,))
-        self.assertTupleEqual(d2._args, (50, self.end))
-        self.assertTupleEqual(d3._args, (0, self.end, 3))
-
-    def test_dunder_iter(self):
-        d1 = Dataset.range(self.end)
-        d2 = Dataset.range(50, self.end)
-        d3 = Dataset.range(0, self.end, 3)
-        self.assertListEqual(d1.all(), list(range(self.end)))
-        self.assertListEqual(d2.all(), list(range(50, self.end)))
-        self.assertListEqual(d3.all(), list(range(0, self.end, 3)))
-
-    def test_range_with_pytorch_dataloader(self):
-        def get_loader(dataset):
-            return DataLoader(dataset,
-                              batch_size=16,
-                              collate_fn=lambda x: x,
-                              shuffle=False,
-                              num_workers=2)
-
-        d1 = Dataset.range(self.end)
-        d2 = Dataset.range(50, self.end)
-        d3 = Dataset.range(0, self.end, 3)
-        loader = get_loader(d1)
-        self.assertListEqual(
-            list(sorted(chain.from_iterable(loader))),
-            list(range(self.end))
-        )
-        loader = get_loader(d2)
-        self.assertListEqual(
-            list(sorted(chain.from_iterable(loader))),
-            list(range(50, self.end))
-        )
-        loader = get_loader(d3)
-        self.assertListEqual(
-            list(sorted(chain.from_iterable(loader))),
-            list(range(0, self.end, 3))
-        )
--- tests/test_text.py
+++ tests/test_text.py
@@ -4,7 +4,7 @@ from itertools import chain
 
 from torch.utils.data import DataLoader
 
-from torchpipe import TextDataset, ConcatTextDataset, ZipTextDataset
+from torchpipe import TextDataset
 
 
 class TextDatasetTestCase(TestCase):
@@ -28,7 +28,7 @@ class TextDatasetTestCase(TestCase):
             self.assertEqual(x, f'line #{str(i).zfill(3)}')
 
     def test_loads_with_torch_dataloader(self):
-        loader = DataLoader(self.data,
+        loader = DataLoader(self.data.parallel(),
                             batch_size=16,
                             collate_fn=lambda x: x,
                             shuffle=False,
@@ -37,93 +37,10 @@ class TextDatasetTestCase(TestCase):
             self.assertEqual(x, f'line #{str(i).zfill(3)}')
 
     def test_loads_with_torch_dataloader_after_shuffle(self):
-        loader = DataLoader(self.data.shuffle(16 * 2),
+        loader = DataLoader(self.data.parallel().shuffle(16 * 2),
                             batch_size=16,
                             collate_fn=lambda x: x,
                             shuffle=False,
                             num_workers=2)
         for x, i in zip(sorted(chain.from_iterable(loader)), range(self.n)):
             self.assertEqual(x, f'line #{str(i).zfill(3)}')
-
-
-class ConcatTextDatasetTestCase(TestCase):
-
-    def setUp(self):
-        self.fp = tempfile.NamedTemporaryFile()
-        self.n = 100
-        for i in range(self.n):
-            self.fp.write(f'line #{str(i).zfill(3)}\n'.encode('utf-8'))
-        self.fp.seek(0)
-        self.data = ConcatTextDataset([self.fp.name, self.fp.name])
-
-    def tearDown(self):
-        self.fp.close()
-
-    def test_dunder_init(self):
-        self.assertListEqual(self.data._paths, [self.fp.name, self.fp.name])
-
-    def test_dunder_iter(self):
-        for x, i in zip(self.data, chain(range(self.n), range(self.n))):
-            self.assertEqual(x, f'line #{str(i).zfill(3)}')
-
-    def test_loads_with_torch_dataloader(self):
-        loader = DataLoader(self.data,
-                            batch_size=16,
-                            collate_fn=lambda x: x,
-                            shuffle=False,
-                            num_workers=2)
-        expected = chain.from_iterable(map(lambda x: [x, x], range(self.n)))
-        for x, i in zip(sorted(chain.from_iterable(loader)), expected):
-            self.assertEqual(x, f'line #{str(i).zfill(3)}')
-
-    def test_loads_with_torch_dataloader_after_shuffle(self):
-        loader = DataLoader(self.data.shuffle(16 * 2),
-                            batch_size=16,
-                            collate_fn=lambda x: x,
-                            shuffle=False,
-                            num_workers=2)
-        expected = chain.from_iterable(map(lambda x: [x, x], range(self.n)))
-        for x, i in zip(sorted(chain.from_iterable(loader)), expected):
-            self.assertEqual(x, f'line #{str(i).zfill(3)}')
-
-
-class ZipTextDatasetTestCase(TestCase):
-
-    def setUp(self):
-        self.fp = tempfile.NamedTemporaryFile()
-        self.n = 100
-        for i in range(self.n):
-            self.fp.write(f'line #{str(i).zfill(3)}\n'.encode('utf-8'))
-        self.fp.seek(0)
-        self.data = ZipTextDataset([self.fp.name, self.fp.name])
-
-    def tearDown(self):
-        self.fp.close()
-
-    def test_dunder_init(self):
-        self.assertListEqual(self.data._paths, [self.fp.name, self.fp.name])
-
-    def test_dunder_iter(self):
-        for x, i in zip(self.data, range(self.n)):
-            line = f'line #{str(i).zfill(3)}'
-            self.assertTupleEqual(x, (line, line))
-
-    def test_loads_with_torch_dataloader(self):
-        loader = DataLoader(self.data,
-                            batch_size=16,
-                            collate_fn=lambda x: x,
-                            shuffle=False,
-                            num_workers=2)
-        for x, i in zip(sorted(chain.from_iterable(loader)), range(self.n)):
-            line = f'line #{str(i).zfill(3)}'
-            self.assertTupleEqual(x, (line, line))
-
-    def test_loads_with_torch_dataloader_after_shuffle(self):
-        loader = DataLoader(self.data.shuffle(16 * 2),
-                            batch_size=16,
-                            collate_fn=lambda x: x,
-                            shuffle=False,
-                            num_workers=2)
-        for x, i in zip(sorted(chain.from_iterable(loader)), range(self.n)):
-            line = f'line #{str(i).zfill(3)}'
-            self.assertTupleEqual(x, (line, line))
